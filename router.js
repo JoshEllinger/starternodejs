e53d6bd602bcbf7d20ef1b83d7da36562ff810a1
Joshua Ellinger <josh.ellinger@gmail.com>
2013-02-22 23:37:05 -0800

    router step 2

---
+++ router.js
@@ -0,0 +1,10 @@
+/**
+ * File: .
+ * User: joshuaellinger
+ * Date: 2/22/13
+ */
+function route(pathname) {
+  console.log("About to route a request for " + pathname + ".");
+}
+
+exports.route = route;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var url = require("url");
 function start() {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
-    console.log("Request for" + pathname + 'recieved.');
+    console.log("Request for " + pathname + ' recieved.');
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello world");
     response.end();
