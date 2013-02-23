b30ae2b02b3a97ea484a1300e539b31b3c876991
Joshua Ellinger <josh.ellinger@gmail.com>
2013-02-22 23:23:35 -0800

    first commit of repo

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * File: .
+ * User: joshuaellinger
+ * Date: 2/22/13
+ * Time: 11:07 PM
+ * To change this template use File | Settings | File Templates.
+ */
+var server = require("./server");
+
+server.start();
