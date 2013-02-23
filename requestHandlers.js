b428e9b2644629b39c143b9ad2b75a165d24c678
Joshua Ellinger <josh.ellinger@gmail.com>
2013-02-23 00:35:38 -0800

    Add some request handlers

---
+++ requestHandlers.js
@@ -0,0 +1,15 @@
+/**
+ * File: .
+ * User: joshuaellinger
+ * Date: 2/23/13
+ */
+function start() {
+  console.log("Request handler 'start' was called.");
+}
+
+function upload() {
+  console.log("Request handler 'upload' was called.");
+}
+
+exports.start = start;
+exports.upload = upload;
