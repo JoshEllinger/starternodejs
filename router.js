1cca8352466478fe1c6699efc09f6c9313112511
Joshua Ellinger <josh.ellinger@gmail.com>
2013-02-23 00:48:04 -0800

    fix some typos

--- router.js
+++ router.js
@@ -5,7 +5,7 @@
  */
 function route(handle, pathname) {
   console.log("About to route a request for " + pathname + ".");
-  if (typeof handle[pathname] === "function") {
+  if (typeof handle[pathname] === 'function') {
     handle[pathname]();
   }
   else {
